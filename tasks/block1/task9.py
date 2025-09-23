2930ad7d48388d1cf74c6e74807ce230382d05fc
Kris465 <nataza7394@gmail.com>
2025-09-23 13:18:32 +0300

    артем

---
+++ tasks/block1/task9.py
@@ -0,0 +1,22 @@
+
+x_a = 10
+x_a = -10
+print("Пункт a: x =", x_a)
+
+
+x_b = 17.5
+x_b = -2 * x_b
+print("Пункт b: x =", x_b)
+
+
+x_c = 60
+x_c = x_c - 1
+x_c = 0
+print("Пункт c: x =", x_c)
+
+
+k = 5
+x_d = -50
+x_d = -25
+x_d = x_d + k
+print("Пункт d: x =", x_d)
