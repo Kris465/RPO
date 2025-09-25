74a3be1e2196fada86dee67829a5469a6c45addc
Kris465 <nataza7394@gmail.com>
2025-09-25 09:35:42 +0300

    artem

---
+++ tasks/block1/task22.py
@@ -0,0 +1,19 @@
+
+x = 2
+
+
+y = 7 * x**2 - 3 * x + 6
+
+
+print(f"При x = {x}, значение функции y = {y}")
+
+def calculate_y(a):
+    y = 2 * 12 * a + 7 * a - 16  
+    return y
+
+
+a = 3  
+
+
+y = calculate_y(a)
+print(f"Значение функции y = 2*12a + 7a - 16 при a = {a} равно {y}")
