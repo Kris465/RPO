a70592c804495fe41e36a2d8560ee05f70241f4a
Kris465 <nataza7394@gmail.com>
2025-09-25 13:52:29 +0300

    Алексей

---
+++ tasks/block1/task37.py
@@ -0,0 +1,8 @@
+from math import sqrt
+
+catheter1 = int(input("Введите значение первого катета: "))
+catheter2 = int(input("Введите значение второго катета: "))
+hypotenuse = catheter1 ** 2 + catheter2 ** 2
+
+perimeter = catheter1 + catheter2 + sqrt(hypotenuse) 
+print(perimeter)
