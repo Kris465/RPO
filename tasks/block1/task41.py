5a2d502d82e15296aaf0d1d89ed6f49899d30700
Kris465 <nataza7394@gmail.com>
2025-09-30 13:57:37 +0300

    фадеев 41

---
+++ tasks/block1/task41.py
@@ -0,0 +1,14 @@
+from math import sqrt, radians, sin, cos
+
+
+e = int(input('Число1 '))
+f = int(input('Число2 '))
+g = int(input('Число3 '))
+h = int(input('Число4 '))
+
+
+a = print(sqrt(abs(e - 3 / f) ** 3) + g)
+
+b = print(sin(radians(e)) + cos(radians(h)) ** 2)
+
+c = print(33 * g / e * f - 3)
