3f7d3eeee100c5f6f3426aeea28ca77221dfc890
Kris465 <nataza7394@gmail.com>
2025-10-07 13:42:22 +0300

    никита

---
+++ tasks/block1/task58.py
@@ -0,0 +1,10 @@
+def print(c):
+    return c * 1.8 + 32
+
+
+def parin(f):
+    return (f - 32) / 1.8
+    (temperature_f) = 451
+    (temperature_c) = (temperature_f)
+    print(f"{temperature_f}°F = {temperature_c:.2f}°C")
+    (f) = 232.78
