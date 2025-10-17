862881afd898fbfb76aee41604577f4aa7020bd2
Kris465 <nataza7394@gmail.com>
2025-10-17 14:05:09 +0300

    Алексей

---
+++ tasks/block3/task3.py
@@ -0,0 +1,11 @@
+X = True
+Y = True
+Z = False
+
+res_a = not X and Y
+res_b = X or not Y
+res_v = X or Y and Z
+
+print("Значение логического выражения А", res_a)
+print("Значение логического выражения Б", res_b)
+print("Значение логического выражения В", res_v)
