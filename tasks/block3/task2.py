6a8b1d92e090ec2b8371056ae37518fc48c72270
Kris465 <nataza7394@gmail.com>
2025-10-17 13:57:38 +0300

    Хеззиев сделал task2.py

---
+++ tasks/block3/task2.py
@@ -0,0 +1,12 @@
+# Задаем логические величины
+X = False  # Ложь
+Y = True   # Истина
+Z = False  # Ложь
+
+result_a = X or Z   # а) X или Z
+result_b = X and Y  # б) X и Y
+result_c = X and Z  # в) X и Z
+
+print("а) X или Z =", result_a)
+print("б) X и Y =", result_b)
+print("в) X и Z =", result_c)
