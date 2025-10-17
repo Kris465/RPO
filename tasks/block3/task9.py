3bd2f3b1cdd07a8d4f3bebe7b909785dc1335911
Kris465 <nataza7394@gmail.com>
2025-10-17 15:05:09 +0300

    Тайцель

---
+++ tasks/block3/task9.py
@@ -0,0 +1,14 @@
+# Присвоение значений
+X = True
+Y = False
+Z = False
+
+# Вычисление выражений
+a = (not X) or (not Y) or (not Z)
+b = (not X or not Y) and (X or Y)
+c = (X and Y) or (X and Z) or (not Z)
+
+# Вывод результатов
+print("а)", a)
+print("б)", b)
+print("в)", c)
