44791b45414ac13f7a489bf30d1cab0b9b35a056
Kris465 <nataza7394@gmail.com>
2025-10-17 10:22:13 +0300

    фадеев 35

---
+++ tasks/block2/task35.py
@@ -0,0 +1,15 @@
+a2 = int(input())
+a1 = int(input())
+b2 = int(input())
+b1 = int(input())
+
+A = 10 * a2 + a1
+B = 10 * b2 + b1
+
+sum_ = A + B
+
+sum_10 = sum_ // 10
+sum_1 = sum_ % 10
+
+print(sum_10)
+print(sum_1)
