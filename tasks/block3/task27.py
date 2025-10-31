23006b14b0426f3a915edb73550476a200dfc90a
Kris465 <nataza7394@gmail.com>
2025-10-31 10:12:37 +0300

    новикова

---
+++ tasks/block3/task27.py
@@ -0,0 +1,47 @@
+x = int(input("Введите число: "))
+y = int(input("Введите число: "))
+
+if x > 2 and y > 3:
+    print("True")
+else:
+    print("False")
+
+if x > 1 or y > -2:
+    print("True")
+else:
+    print("False")
+
+if x >= 0 and y < 5:
+    print("True")
+else:
+    print("False")
+
+if x > 3 or x < 10:
+    print("True")
+else:
+    print("False")
+
+if x > 3 and x < 10:
+    print("True")
+else:
+    print("False")
+
+if x <= 2:
+    print("True")
+else:
+    print("False")
+
+if x <= 0 and x >= 5:
+    print("True")
+else:
+    print("False")
+
+if 10 < x and x <= 20:
+    print("True")
+else:
+    print("False")
+
+if 0 < y and y <= 4 and x < 5:
+    print("True")
+else:
+    print("False")
