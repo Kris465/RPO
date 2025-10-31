36495da1937f62645ad028f00ade321d4d1b0035
Kris465 <nataza7394@gmail.com>
2025-10-31 11:47:58 +0300

    Тайцель

---
+++ tasks/block4/task4.py
@@ -0,0 +1,12 @@
+def determine_area(x, y):
+
+    if y < x:
+        return "Область I"
+    else:
+        return "Область II"
+
+x = float(input("Введите x: "))
+y = float(input("Введите y: "))
+
+result = determine_area(x, y)
+print(result)
